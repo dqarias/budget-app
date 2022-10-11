cf88f98a6fa8c2e30d44be1a74ba76f044b566fc
dqarias <dino.quispea@gmail.com>
2022-10-10 19:22:55 -0500

    Update join table

--- db/migrate/20221011002128_create_join_table_category_operation.rb
+++ db/migrate/20221011002128_create_join_table_category_operation.rb
@@ -1,4 +1,4 @@
-class CreateJoinTableCategoriesOperations < ActiveRecord::Migration[7.0]
+class CreateJoinTableCategoryOperation < ActiveRecord::Migration[7.0]
   def change
     create_join_table :categories, :operations do |t|
       # t.index [:category_id, :operation_id]

84b8f5dbe4db980290881f74762a660ddb442a9c
dqarias <dino.quispea@gmail.com>
2022-10-12 17:06:38 -0500

    Update routes for only some operations on categories and operations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  resources :categories do
-    resources :operations
+  resources :categories, only: [:index, :new, :create] do
+    resources :operations, only: [:index, :new, :create]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
